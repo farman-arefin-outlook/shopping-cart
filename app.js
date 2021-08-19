01c4b0d39a9ee9e42685e38abe9b555127f93db3
Tamim <farman.arefin1@outlook.com>
2021-08-19 15:19:15 +0600

    Added updateProductNumber

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ function incrementDecrement(isIncrement, product, productInput) {
 } */
 //Update case number
 
-function updateCaseNumber(isAdd, product, price) {
+function updateProductNumber(isAdd, product, price) {
+    debugger;
     const inputField = document.getElementById(product + '-input-field');
     let inputFieldValue = inputField.value;
     if (isAdd) {
@@ -26,9 +27,8 @@ function updateCaseNumber(isAdd, product, price) {
 
 
     //update product price
-    const productPrice = document.getElementById(product + '-total');
-    const productPriceNum = parseFloat(productPrice);
-    productPrice.innerText = inputFieldValue * price;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = inputFieldValue * price;
 }
 
 //This is phone section
@@ -37,20 +37,20 @@ document.getElementById('phone-plus-btn').addEventListener('click', function () 
     const inputFieldPlusValue = inputPlusField.value;
     inputPlusField.value = parseInt(inputFieldPlusValue) + 1; */
 
-    updateCaseNumber(true, 'phone', 1219);
+    updateProductNumber(true, 'phone', 1219);
 });
 
 document.getElementById('phone-minus-btn').addEventListener('click', function () {
-    updateCaseNumber(false, 'phone', 1219);
+    updateProductNumber(false, 'phone', 1219);
 });
 
 
 //This is phone-casing section
 
 document.getElementById('case-plus-btn').addEventListener('click', function () {
-    updateCaseNumber(true, 'case', 59);
+    updateProductNumber(true, 'case', 59);
 });
 
 document.getElementById('case-minus-btn').addEventListener('click', function () {
-    updateCaseNumber(false, 'case', 59);
+    updateProductNumber(false, 'case', 59);
 });
